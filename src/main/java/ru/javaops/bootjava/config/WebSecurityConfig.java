15a88385262439982a1654cfe1457568a2f779cb
Lokuster <Lokuster11311@mail.ru>
2023-01-06 14:45:37 +0300

    4_05_role_auth.patch

--- src/main/java/ru/javaops/bootjava/config/WebSecurityConfig.java
+++ src/main/java/ru/javaops/bootjava/config/WebSecurityConfig.java
@@ -6,15 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.web.SecurityFilterChain;
 import ru.javaops.bootjava.AuthUser;
 import ru.javaops.bootjava.model.User;
 import ru.javaops.bootjava.repository.UserRepository;
 
 import java.util.Optional;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
@@ -37,5 +41,17 @@ public class WebSecurityConfig {
     public void configureGlobal(AuthenticationManagerBuilder auth, UserDetailsService userDetailsService) throws Exception {
         auth.userDetailsService(userDetailsService)
                 .passwordEncoder(PasswordEncoderFactories.createDelegatingPasswordEncoder());
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/api/account").hasRole("USER")
+                        .requestMatchers("/api/**").hasRole("ADMIN")
+                        .anyRequest().authenticated())
+                .httpBasic(withDefaults())
+                .formLogin(withDefaults());
+        return http.build();
     }
 }
